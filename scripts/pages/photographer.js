2e5ad4f37eea6bd72671996cf5bdb472df852945
David <david.houesnard@gmail.com>
2023-01-08 01:15:50 +0100

    Update photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,14 +2,19 @@ async function getPhotographers() {
   // récupère les données depuis le fichier json
   return fetch("data/photographers.json").then((response) => response.json());
 }
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const pageId = params.get("photographer");
 
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photograph-header");
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerPage(photographer);
-    const userCardDOM = photographerModel.getUserCardPage();
-    photographersSection.appendChild(userCardDOM);
-  });
+  photographers
+    .filter((photographer) => photographer.id == pageId)
+    .forEach((photographer) => {
+      const photographerModel = photographerPage(photographer);
+      const userCardDOM = photographerModel.getUserCardPage();
+      photographersSection.appendChild(userCardDOM);
+    });
 }
 
 async function init() {
